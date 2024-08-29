81633bc2b485df49655db2563637858d0dd06bea
Тимченко Влад <timchenko.vld@gmail.com>
2024-08-29 14:53:47 +0300

    Setting up the sql database

---
+++ app/src/main/java/com/example/cleanarchitecturenoteapp/feature_note/domain/model/Note.kt
@@ -0,0 +1,23 @@
+package com.example.cleanarchitecturenoteapp.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.cleanarchitecturenoteapp.ui.theme.BabyBlue
+import com.example.cleanarchitecturenoteapp.ui.theme.LightGreen
+import com.example.cleanarchitecturenoteapp.ui.theme.RedOrange
+import com.example.cleanarchitecturenoteapp.ui.theme.RedPink
+import com.example.cleanarchitecturenoteapp.ui.theme.Violet
+
+@Entity
+data class Note(
+    @PrimaryKey
+    val id: Int? = null,
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int
+) {
+    companion object {
+        val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
+    }
+}
